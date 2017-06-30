0304e52bcac1625e05cdff88f94e7000a6010600
NathalyInlimit <nathaly@inlimit.net>
2017-06-30 15:52:30 -0500

    masterseller

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,13 @@ for (i = 0; i < acc.length; i++) {
     this.classList.toggle("active");
   }
 }
+
+
+function clock(numeroID) {
+ $("#clockdiv"+numeroID)
+   .countdown("2017/07/24", function(event) {
+     $(this).text(
+       event.strftime('%D d %H h %M m %S s')
+     );
+   });
+}
